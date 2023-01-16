2889abc24e19fa873572e9d887210610a9439afa
CaioSC01 <caio180g@gmail.com>
2023-01-16 17:46:56 -0300

    configuramdo login

--- src/pages/Entrada/Home.tsx
+++ src/pages/Entrada/Home.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const Home = () => {
-	return (
-		<div className='main-login'>
-			<div className='left-login'>
-				<h1>Desafio Sharenergy</h1>
-			</div>
-			<div className='right-login'>
-				<div className="card-login"></div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="main-login">
+      <div className="left-login">
+        <h1>Desafio Sharenergy</h1>
+      </div>
+      <div className="right-login">
+        <div className="card-login">
+          <div className="textfield">
+            <label htmlFor="usuario">Usuario</label>
+            <input type="text" name="usuario" placeholder="Usuario" />
+          </div>
+          <div className="textfield">
+            <label htmlFor="senha">Senha</label>
+            <input type="password" name="senha" placeholder="Senha" />
+          </div>
+					<button className="btn-login">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
