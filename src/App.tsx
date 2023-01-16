848c2f6ca1dd62b92825383fe4872115e4057521
CaioSC01 <caio180g@gmail.com>
2023-01-16 17:27:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+import { Router } from "./server/router";
+
+export const App = () => {
+  return (
+		<>
+		<Router/>
+		</>
+	);
+};
---
+++ src/pages/Entrada/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Home = () => {
+	return (
+		<div className='main-login'>
+			<div className='left-login'>
+				<h1>Desafio Sharenergy</h1>
+			</div>
+			<div className='right-login'>
+				<div className="card-login"></div>
+			</div>
+		</div>
+	)
+}
