449906e7cc0ef67a1366dcdf9744f3f30cd6c1cf
CaioSC01 <caio180g@gmail.com>
2023-01-17 04:43:26 -0300

    estilização e mudanças home

--- src/pages/Entrada/Home.tsx
+++ src/pages/Entrada/Home.tsx
@@ -1,22 +1,20 @@
 import React from "react";
+import { Login } from "../Login/Login";
+import "./home.css";
 
 export const Home = () => {
   return (
     <div className="main-login">
       <div className="left-login">
-        <h1>Desafio Sharenergy</h1>
+        <h1 className="typing-animation">
+          Esse é um desafio <br /> feito para a empresa Sharenergy.
+        </h1>
+				<img src="Imageilustre.svg" className="left-login-image " alt="imagem ilustrativa" />
+	
       </div>
       <div className="right-login">
         <div className="card-login">
-          <div className="textfield">
-            <label htmlFor="usuario">Usuario</label>
-            <input type="text" name="usuario" placeholder="Usuario" />
-          </div>
-          <div className="textfield">
-            <label htmlFor="senha">Senha</label>
-            <input type="password" name="senha" placeholder="Senha" />
-          </div>
-					<button className="btn-login">Login</button>
+          <Login />
         </div>
       </div>
     </div>
