83864be3a6cf7d29aeb83e7fbb71153bf0a19ecd
CaioSC01 <caio180g@gmail.com>
2023-01-19 05:17:45 -0300

    criando paginas novas

---
+++ src/pages/Terceira Pagina/Random.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Theme } from '../../components/Theme/Theme'
+
+export const Random = () => {
+	return (
+		<Theme>
+		<div className="flex items-center space-x-4 sm:px-20 ">
+			<h2 className="py-4 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+			 
+			</h2>
+		</div>
+		{/* TABELA/CONTEUDO/CARDS... */}
+		<div className="  sm:ml-10 ">
+	aaaa
+		</div>
+</Theme>
+	)
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavBar } from "../components/Theme/Navbar/Index";
 import { Home } from "../pages/Entrada/Home";
 import { ListaUsuarios } from "../pages/Pagina inicial/ListaUsuarios";
+import { Status } from "../pages/Segunda Pagina/Status";
+import { Random } from "../pages/Terceira Pagina/Random";
 
 export const Router = () => {
   return (
@@ -9,6 +11,8 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<NavBar children={undefined} />} />
         <Route path="/listausuario" element={<ListaUsuarios />} />
+				<Route path="/status" element={<Status />} />
+				<Route path="/random" element={<Random />} />
       </Routes>
     </BrowserRouter>
   );
